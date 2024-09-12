52b8adce13af77cac8b34bfcd2824234122ab211
SychAndrii <asych@myseneca.ca>
2024-09-12 13:47:53 -0400

    Finding existing files and throwing an error if a file is not found

--- src/controllers/click_controller.py
+++ src/controllers/click_controller.py
@@ -3,10 +3,18 @@ from src import __version__
 from .helpers import CustomCommand
 
 class ClickController:
+
     @click.command(cls=CustomCommand)
+    @click.argument(
+        "file_paths", nargs=-1, type=click.Path(exists=True)
+    )  # Accept multiple file paths
     @click.option("-v", "--version", is_flag=True, help="Show the version and exit.")
     @click.pass_context
-    def hello(ctx, version):
+    def hello(ctx, file_paths, version):
         if version:
             click.echo(__version__)
             ctx.exit()
+
+        # Process each file path
+        for file_path in file_paths:
+            click.echo(f"Processing file: {file_path}")
