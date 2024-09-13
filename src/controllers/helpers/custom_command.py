40c285929ce79e2c892a7276a6411c9bdf928572
SychAndrii <asych@myseneca.ca>
2024-09-13 17:51:20 -0400

    Changed description of -v and -h flags

--- src/controllers/helpers/custom_command.py
+++ src/controllers/helpers/custom_command.py
@@ -1,8 +1,9 @@
 import click
 
+
 class CustomCommand(click.Command):
     """
-    A custom command class that extends `click.Command` to modify the behavior of 
+    A custom command class that extends `click.Command` to modify the behavior of
     the `--help` option in Click commands.
 
     This class introduces the following changes:
@@ -12,27 +13,20 @@ class CustomCommand(click.Command):
 
     def parse_args(self, ctx, args):
         """
-        Parses the command-line arguments and ensures that the `-h` flag is 
+        Parses the command-line arguments and ensures that the `-h` flag is
         interpreted as `--help`.
-
-        This method modifies the argument list to replace `-h` with `--help`, 
-        ensuring that the user gets the help message when using `-h` instead 
-        of `--help`.
         """
-
         if "-h" in args:
             args = ["--help" if arg == "-h" else arg for arg in args]
         return super().parse_args(ctx, args)
 
     def get_help_option(self, ctx):
         """
-        Overrides the default help option behavior to add `-h` as an alias for `--help`.
-
-        This method retrieves the help option from the base `click.Command` class,
-        and then appends `-h` to the list of help flag options, making it a valid
-        flag for displaying the help message.
+        Overrides the default help option behavior to customize the text
+        displayed for the `-h` and `--help` options.
         """
-        # Override to add `-h` as an alias for `--help` in the help message
         help_option = super().get_help_option(ctx)
-        help_option.opts.append("-h")  # Add `-h` to the help flag options
+        if help_option:
+            # Override the help text for the `-h` and `--help` options
+            help_option.help = "Show the help message."
         return help_option
